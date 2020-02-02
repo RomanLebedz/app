dee9bdf0ca300e4b03037ba353a2ec6d201e2e50
Hudkov Kolya <kgudkov@redcapcloud.com>
2020-02-02 21:09:38 +0300

    add main page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Button from '@material-ui/core/Button';
+import MainMenu from "./MainMenu";
+
+import {createMuiTheme} from '@material-ui/core/styles';
+import {ThemeProvider} from "@material-ui/styles";
+import Grid from "@material-ui/core/Grid";
+import Item from "./Item";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#c5cae9',
+        },
+        secondary: {
+            light: '#0066ff',
+            main: '#0044ff',
+            contrastText: '#ffcc00',
+        },
+    },
+});
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        height: 140,
+        width: 100,
+    }, control: {
+        padding: theme.spacing(2),
+    },
+    card: {
+        margin: '7px'
+    }
+}));
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Button variant="contained">Default</Button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const classes = useStyles();
+    return (
+        <ThemeProvider theme={theme}>
+            <div className="App">
+                <MainMenu/>
+                <Grid container className={classes.root}>
+                        <Grid container justify="center" spacing={12}>
+                            {[0, 1, 2, 3, 4, 5, 6, 76, 7, 8].map(value => (
+                                <Grid key={value} item className={classes.card}>
+                                    <Item/>
+                                </Grid>
+                            ))}
+                        </Grid>
+                </Grid>
+            </div>
+        </ThemeProvider>
+    );
 }
 
 export default App;
